8af8fb59c3c5e01f186da6175a0ef57bb79f026d
VincenzoBorriello89 <vincborr89@gmail.com>
2022-05-10 16:16:04 +0200

    esercizio completato

--- index.php
+++ index.php
@@ -22,28 +22,46 @@ class Movie {
         $this -> produzione = $_produzione;
     }
 
+    // definisco dei metodi cioè delle funzioni
+    public function getTitolo(){
+        return $this -> titolo;
+    }
+
+    public function getAnno(){
+        return $this -> anno;
+    }
+
+    public function getRegista(){
+        return $this -> regista;
+    }
+
+    public function getProduzione(){
+        return $this -> produzione;
+    }
+
 }
 
 // istanziamo il primo oggetto
 $avatar = new Movie("Avatar", 2009, "James Cameron", "20th Century Fox");
 
 // stampa a schermo del primo oggetto
-echo "<p>" . $avatar -> titolo . "</p>";
-echo "<p>" . $avatar -> anno . "</p>";
-echo "<p>" . $avatar -> regista . "</p>";
-echo "<p>" . $avatar -> produzione . "</p>";
+echo "<p>Titolo: " . $avatar -> titolo . "</p>";
+echo "<p>Anno: " . $avatar -> anno . "</p>";
+echo "<p>Regista: " . $avatar -> regista . "</p>";
+echo "<p>Casa di Produzione: " . $avatar -> produzione . "</p>";
 
 echo $avatar -> titolo . " - " . $avatar -> anno . " - " . $avatar -> regista . " - " . $avatar -> produzione . "</p>";
 
 // istanziamo il secondo oggetto
 $avengers = new Movie("Avengers: Endgame", 2019, "Anthony e Joe Russo", "Marvel Studios");
 
-// stampa a schermo del secondo oggetto
-echo "<p>" . $avengers -> titolo . "</p>";
-echo "<p>" . $avengers -> anno . "</p>";
-echo "<p>" . $avengers -> regista . "</p>";
-echo "<p>" . $avengers -> produzione . "</p>";
+// stampa a schermo del secondo oggetto usando i metodi, cioè le funzioni
+echo "<p>-------------------------------------------------</p>";
+echo "<p>Titolo: " . $avengers -> getTitolo() . "</p>";
+echo "<p>Anno: " . $avengers -> getAnno() . "</p>";
+echo "<p>Regista: " . $avengers -> getRegista() . "</p>";
+echo "<p>Casa di Produzione: " . $avengers -> getProduzione() . "</p>";
 
-echo $avengers -> titolo . " - " . $avengers -> anno . " - " . $avengers -> regista . " - " . $avengers -> produzione . "</p>";
+echo $avengers -> getTitolo() . " - " . $avengers -> getAnno() . " - " . $avengers -> getRegista() . " - " . $avengers -> getProduzione() . "</p>";
 
 ?>
